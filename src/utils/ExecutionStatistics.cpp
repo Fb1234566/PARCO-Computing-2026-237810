ab69e957df2cffc560d97ad77a7e72e503379b12
Filippo Benedetti <filippo.benedetti3@gmail.com>
2025-10-27 11:50:32 +0100

    Added avg computation time

--- src/utils/ExecutionStatistics.cpp
+++ src/utils/ExecutionStatistics.cpp
@@ -27,6 +27,7 @@ void utils::ExecutionStatistics::run() const {
     // numero di iterazioni per il benchmark (best of N)
     const int iters = 10;
     double best_ms = 1e9;
+    double sum_ms = 0;
 
     for (int t = 0; t < iters; ++t) {
         auto t0 = std::chrono::steady_clock::now();
@@ -35,10 +36,15 @@ void utils::ExecutionStatistics::run() const {
 
         double ms = std::chrono::duration<double, std::milli>(t1 - t0).count();
         if (ms < best_ms) best_ms = ms;
+        sum_ms+=ms;
     }
 
     // stampa risultato
     std::string msg = "Best computation time: " + std::to_string(best_ms) + " ms";
+    std::string avg = "Average computation time: " + std::to_string(sum_ms/iters) + " ms";
+
     utils::PrintUtils::printColored(msg, utils::PrintUtils::TerminalColor::GREEN);
+    utils::PrintUtils::printColored(avg, utils::PrintUtils::TerminalColor::GREEN);
+
 }
 
