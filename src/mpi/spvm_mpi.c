ec4b7c7495ffc804ceb0fb7eba11a4408efef662
Filippo Benedetti <filippo.benedetti3@gmail.com>
2026-01-26 18:30:01 +0100

    Added nprocs to export table

--- src/mpi/spvm_mpi.c
+++ src/mpi/spvm_mpi.c
@@ -22,6 +22,7 @@ void print_help(char *prog_name) {
     printf("  --export <path> Path to result file\n");
     printf("  --iteration <int> Path to result file\n");
 	printf("  --help          Show this help message\n");
+	printf("  --nprocs <int> Number of processros\n");
 }
 
 typedef struct CSRheader{
@@ -63,6 +64,7 @@ int main(int argc, char **argv) {
         int syn_cols = 1000;
         int syn_nnz = 5000;
 		int iteration = 0;
+		int nprocs = -1;
         // Simple manual parsing
         for (int i = 1; i < argc; i++) {
             if (strcmp(argv[i], "--help") == 0) {
@@ -83,6 +85,8 @@ int main(int argc, char **argv) {
                 export_path = argv[++i];
             } else if (strcmp(argv[i], "--iteration") == 0 && i + 1 < argc) {
                 iteration = atoi(argv[++i]);
+            } else if (strcmp(argv[i], "--nprocs") == 0 && i + 1 < argc) {
+                nprocs = atoi(argv[++i]);
             }
 
         }
@@ -305,12 +309,13 @@ int main(int argc, char **argv) {
 
             /* Initialize header with 3 columns */
             Header h;
-            h.count = 3;
+            h.count = 4;
             h.s = malloc(sizeof(char*) * h.count);
             if (!h.s) { perror("malloc"); return 1; }
             h.s[0] = strdup("Time");
             h.s[1] = strdup("Iteration");
             h.s[2] = strdup("Status");
+			h.s[3] = strdup("NProc");
 
             /* Write header (only writes if file is empty) */
 
