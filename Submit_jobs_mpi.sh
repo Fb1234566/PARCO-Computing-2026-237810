2a5ad345bd1340744953d934b57fb142153546f4
Filippo Benedetti <filippo.benedetti3@gmail.com>
2026-02-04 21:33:21 +0100

    modified the run executables

--- Submit_jobs_mpi.sh
+++ Submit_jobs_mpi.sh
@@ -17,8 +17,13 @@ fi
 MPI_SIZES=(1 2 4 8 16 32 64 128 256)
 
 
-# Build MPI target and synthetic matrix generator
 make clean
+
+echo "Building binaries"
+make mpi
+make serial-only
+make openmp-static-only
+
 
 # Create a Python virtual environment (for analysis) and install minimal deps
 if [ ! -d "./.venv" ]; then
